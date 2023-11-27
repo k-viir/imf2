016f6ab89787c0a854010cc9f526b536abd6d6f7
k-viir <98878030+k-viir@users.noreply.github.com>
2023-11-28 00:34:15 +0100

    finished implementation of all crud operations

--- Acceso a Datos/Practica 2 Archivos/jvdb.c
+++ Acceso a Datos/Practica 2 Archivos/jvdb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 void printFail()
 {
@@ -9,7 +10,7 @@ void printFail()
 
 void printSuccess()
 {
-    printf("Archivo escrito correctamente \n");
+    printf("Operación realizada con exito \n");
 }
 
 // Funcion SELECT
@@ -64,18 +65,184 @@ int insertFunction(char *search, FILE *archivo, char *ruta, char *newValue)
     }
 }
 // FUNCION DELETE (Se eliminaran todas las coincidencias)
-int deleteFunction(char *search, FILE *archivo, char *ruta, char *newValue)
+// jvdb.exe delete clientes  Cavirr NULL
+// jvdb.exe delete clientes  all NULL
+
+int deleteFunction(char *searchString, FILE *archivo, char *ruta, char *newValue)
 {
+    FILE *tempArchivo = fopen("temp/temp.txt", "a");
+    archivo = fopen(ruta, "r");
+    char linea[1024];
+    char lineaTemp[1024];
+    if (strcmp(searchString, "all") == 0)
+    {
+        fclose(archivo);
+        fclose(tempArchivo);
+
+        remove(ruta);
+
+        printf("Borrando todos los registros");
+        FILE *nuevoArchivo = fopen(ruta, "a");
+        fclose(nuevoArchivo);
+        // leyendo el archivo temporal y haciendo traspaso
+        free(ruta);
+
+        printSuccess();
+
+        // lógica para guardar el archivo vacío
+    }
+    else
+    {
+        while (fgets(linea, sizeof(linea), archivo) != NULL)
+        {
+            // validando que el último caracter no sea salto de linea para hacer bien la comparacion
+            int length = strlen(linea);
+            if (linea[length - 1] == '\n')
+            {
+                linea[length - 1] = 0;
+            }
+            // si el registro no coincide se guarda en el archivo temporal
+            if (strcmp(searchString, linea) != 0)
+            {
+                fputs(strcat(linea, "\n"), tempArchivo);
+            }
+        }
+
+        fclose(archivo);
+        fclose(tempArchivo);
+
+        remove(ruta);
+
+        FILE *nuevoArchivo = fopen(ruta, "a");
+        tempArchivo = fopen("temp/temp.txt", "r");
+
+        // leyendo el archivo temporal y haciendo traspaso
+        while (fgets(lineaTemp, sizeof(lineaTemp), tempArchivo) != NULL)
+        {
+            // pasando los registros finales al nuevo archivo que sustituirá al original
+            fputs(lineaTemp, nuevoArchivo);
+        }
+        fclose(tempArchivo);
+        fclose(nuevoArchivo);
+        remove("temp/temp.txt");
+
+        // liberar memoria
+        free(ruta);
+
+        printSuccess();
+    }
+
     return 0;
 }
 // FUNCION UPDATE (Se actualizarán todas las coincidencias)
-int updateFunction(char *search, FILE *archivo, char *ruta, char *newValue)
+int updateFunction(char *searchString, FILE *archivo, char *ruta, char *newValue)
 {
+    FILE *tempArchivo = fopen("temp/temp.txt", "a");
+    archivo = fopen(ruta, "r");
+    char linea[1024];
+    char lineaTemp[1024];
+
+    while (fgets(linea, sizeof(linea), archivo) != NULL)
+    {
+        // validando que el último caracter no sea salto de linea para hacer bien la comparacion
+        int length = strlen(linea);
+        if (linea[length - 1] == '\n')
+        {
+            linea[length - 1] = 0;
+        }
+        // si el registro coincide se guarda en el archivo temporal el nuevo valor
+        if (strcmp(searchString, linea) == 0)
+        {
+            fputs(strcat(newValue, "\n"), tempArchivo);
+        }
+        else
+        { // si no coincide se guarda el valor antiguo
+            fputs(strcat(linea, "\n"), tempArchivo);
+        }
+    }
+
+    fclose(archivo);
+    fclose(tempArchivo);
+
+    remove(ruta);
+
+    FILE *nuevoArchivo = fopen(ruta, "a");
+    tempArchivo = fopen("temp/temp.txt", "r");
+
+    // leyendo el archivo temporal y haciendo traspaso
+    while (fgets(lineaTemp, sizeof(lineaTemp), tempArchivo) != NULL)
+    {
+        // pasando los registros finales al nuevo archivo que sustituirá al original
+        fputs(lineaTemp, nuevoArchivo);
+    }
+    fclose(tempArchivo);
+    fclose(nuevoArchivo);
+    remove("temp/temp.txt");
+
+    // liberar memoria
+    free(ruta);
+
+    printSuccess();
+
     return 0;
 }
 // FUNCION UPSERT
-int upsertFunction(char *search, FILE *archivo, char *ruta, char *newValue)
+int upsertFunction(char *searchString, FILE *archivo, char *ruta, char *newValue)
 {
+    FILE *tempArchivo = fopen("temp/temp.txt", "a");
+    archivo = fopen(ruta, "r");
+    char linea[1024];
+    char lineaTemp[1024];
+    bool foundMatch = false;
+
+    while (fgets(linea, sizeof(linea), archivo) != NULL)
+    {
+        // validando que el último caracter no sea salto de linea para hacer bien la comparacion
+        int length = strlen(linea);
+        if (linea[length - 1] == '\n')
+        {
+            linea[length - 1] = 0;
+        }
+        // si el registro coincide se guarda en el archivo temporal el nuevo valor
+        if (strcmp(searchString, linea) == 0)
+        {
+            fputs(strcat(newValue, "\n"), tempArchivo);
+            foundMatch = true;
+        }
+        else
+        { // si no coincide se guarda el valor antiguo
+            fputs(strcat(linea, "\n"), tempArchivo);
+        }
+    }
+    // si no se ha encontrado ninguna coincidencia al terminar la búsqueda, se inserta el nuevo valor (el valor buscado)
+    if (foundMatch == false)
+    {
+        fputs(strcat(searchString, "\n"), tempArchivo);
+    }
+
+    fclose(archivo);
+    fclose(tempArchivo);
+
+    remove(ruta);
+
+    FILE *nuevoArchivo = fopen(ruta, "a");
+    tempArchivo = fopen("temp/temp.txt", "r");
+
+    // leyendo el archivo temporal y haciendo traspaso
+    while (fgets(lineaTemp, sizeof(lineaTemp), tempArchivo) != NULL)
+    {
+        // pasando los registros finales al nuevo archivo que sustituirá al original
+        fputs(lineaTemp, nuevoArchivo);
+    }
+    fclose(tempArchivo);
+    fclose(nuevoArchivo);
+    remove("temp/temp.txt");
+
+    // liberar memoria
+    free(ruta);
+
+    printSuccess();
+
     return 0;
 }
 
@@ -91,7 +258,7 @@ int main(int argc, char *argv[])
     FILE *archivo;
 
     char *ruta;
-    // allocate, asignar memoria de forma dinamica con tamaño de strings + '/' y null fin
+    // allocate, asignar memoria de forma dinamica con tamaño de strings + '/' y null final
     // char *ruta = malloc(strlen(directorio) + strlen(baseDatos) + 2);
 
     // generar la ruta con el / de por medio (por formato)
@@ -108,6 +275,10 @@ int main(int argc, char *argv[])
     else if (strcmp(operacion, "delete") == 0)
     {
         deleteFunction(search, archivo, ruta, newValue);
+    }
+    else if (strcmp(operacion, "update") == 0)
+    {
+        updateFunction(search, archivo, ruta, newValue);
     }
     else if (strcmp(operacion, "upsert") == 0)
     {
